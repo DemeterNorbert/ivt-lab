7666ccb6a32849757b052ade32f1499752f0b6a7
Norbi <demnorbi@gmail.com>
2021-05-07 14:17:24 +0200

    Mockito when and verify are used for tests.

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,46 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore ts1;
+  private TorpedoStore ts2;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500(mock(TorpedoStore.class), mock(TorpedoStore.class));
+    ts1 = mock(TorpedoStore.class);
+    ts2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(ts1, ts2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(ts1.isEmpty()).thenReturn(false);
+    when(ts1.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(ts1, times(1)).isEmpty();
+    verify(ts1, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(ts1.isEmpty()).thenReturn(false);
+    when(ts1.fire(1)).thenReturn(true);
+    when(ts2.isEmpty()).thenReturn(false);
+    when(ts2.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(ts1, times(1)).isEmpty();
+    verify(ts1, times(1)).fire(1);
+    verify(ts2, times(1)).isEmpty();
+    verify(ts2, times(1)).fire(1);
   }
 
 }
